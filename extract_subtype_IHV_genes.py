883b257d7c6984da3ac9d746ce1c01bbbd998cbe
Ed Mountjoy <emountjoy@gmail.com>
2014-02-09 17:29:52 +0000

    done

---
+++ extract_subtype_IHV_genes.py
@@ -0,0 +1,102 @@
+# pypy script <in_fasta> <out_fasta>
+
+import sys
+from math import ceil
+
+def main(args):
+    
+    gene_set = set([])
+    
+    with open(args['in_list'], 'r') as in_list:
+        for line in in_list:
+            gene = line.rstrip('\n')
+            gene_set.add(gene)
+    
+    with open(args['out'], 'w') as out_handle:
+        for record in fasta_parser(open(args['in_fasta'], 'r')):
+            if record[0] in gene_set:
+                write_fasta(out_handle, record[0], record[1])
+
+def fasta_parser(handle):
+    """Generator function to iterator over Fasta records (as string tuples).
+
+    For each record a tuple of two strings is returned, the FASTA title
+    line (without the leading '>' character), and the sequence (with any
+    whitespace removed). The title line is not divided up into an
+    identifier (the first word) and comment or description.
+
+    >>> for values in SimpleFastaParser(open("Fasta/dups.fasta")):
+    ...     print(values)
+    ('alpha', 'ACGTA')
+    ('beta', 'CGTC')
+    ('gamma', 'CCGCC')
+    ('alpha (again - this is a duplicate entry to test the indexing code)', 'ACGTA')
+    ('delta', 'CGCGC')
+
+    """
+    #Skip any text before the first record (e.g. blank lines, comments)
+    while True:
+        line = handle.readline()
+        if line == "":
+            return  # Premature end of file, or just empty?
+        if line[0] == ">":
+            break
+
+    while True:
+        if line[0] != ">":
+            raise ValueError(
+                "Records in Fasta files should start with '>' character")
+        title = line[1:].rstrip()
+        lines = []
+        line = handle.readline()
+        while True:
+            if not line:
+                break
+            if line[0] == ">":
+                break
+            lines.append(line.rstrip())
+            line = handle.readline()
+
+        #Remove trailing whitespace, and any internal spaces
+        #(and any embedded \r which are possible in mangled files
+        #when not opened in universal read lines mode)
+        yield title, "".join(lines).replace(" ", "").replace("\r", "")
+
+        if not line:
+            return  # StopIteration
+
+def write_fasta(handle, header, seq, max_line=79):
+    """ Will write the fasta sequence to the handle.
+    """
+    
+    # Calculate how many columns per line
+    num_lines = int(len(seq) / max_line) + 1
+    len_line = int(ceil(float(len(seq))/num_lines))
+    
+    # Write header
+    handle.write('>')
+    handle.write(header)
+    handle.write('\n')
+    
+    # Write wrapped lines
+    for seq_part in wrap(seq, len_line):
+        handle.write(seq_part)
+        handle.write('\n')
+        
+    return 0
+
+def wrap(string, length):
+    """ Yield successive length-sized chunks from string.
+    """
+    for i in xrange(0, len(string), length):
+        yield string[i:i + length]
+
+if __name__ == '__main__':
+    
+    args = {}
+    
+    args['in_fasta'] = sys.argv[1]
+    args['in_list'] = sys.argv[2]
+    args['out'] = sys.argv[3]
+    
+    main(args)
---
+++ process_sam.py
@@ -0,0 +1,186 @@
+# pypy script <in_fasta> <out_fasta>
+
+import sys
+import pysam
+
+class Read:
+    
+    def __init__(self, query_name):
+        self.query_name = str(query_name)
+        self.j_ref = None
+        self.v_ref = None
+    
+    def parse_J_attr(self, alignment):
+        # Add reference match to list
+        self.j_ref = int(alignment.rname)
+        # J deletion size
+        self.j_del = int(alignment.pos)
+        # 0 based index of the first insert base
+        self.insert_end = int(alignment.qstart - 1)
+
+    def parse_V_attr(self, alignment, ref_len):
+        # Add reference match to list
+        self.v_ref = int(alignment.rname)
+        # V deletion size
+        self.v_del = int(ref_len - (alignment.aend + 1))
+        # 0 based index of the last insert base
+        self.insert_start = int(alignment.qend + 1)
+
+class Group:
+    
+    def __init__(self, parent_read):
+        self.j = parent_read.j_ref
+        self.v = parent_read.v_ref
+        self.members = [parent_read]
+    
+    def check_membership(self, new_read):
+        # If read shares the same V and J, add to group and return True
+        if new_read.j_ref == self.j and new_read.v_ref == self.v:
+            self.members.append(new_read)
+            return True
+        else:
+            return False
+
+def main(args):
+    
+    print 'Parsing SAMs...'
+    read_dict, ref_names = parse_sams(args['J_in'], args['V_in'])
+    
+    #~ # Output clone-specific regions
+    print 'Writing fasta...'
+    with open('clone_specific_seqs.fasta', 'w') as out_handle:
+        with pysam.Samfile(args['J_in'], 'r') as sam_file:
+                # Go through each read in the alignment
+                for alignedread in sam_file.fetch():
+                    if alignedread.is_unmapped:
+                        continue
+                    record = read_dict[alignedread.qname]
+                    try:
+                        ins_start = record.insert_start
+                    except:
+                        ins_start = 0
+                    try:
+                        ins_end = record.insert_end + 1
+                    except:
+                        ins_end = -1
+                    
+                    # Write fasta
+                    out_handle.write('>{0}\n'.format(record.query_name))
+                    seq = alignedread.seq[ins_start:ins_end]
+                    out_handle.write('{0}\n'.format(seq))
+
+
+
+def group_reads():
+    """ Not yet implemented
+    """
+    #~ i = 0
+    #~ tot_reads = len(read_dict)
+    #~ # Group reads by J and V
+    #~ print 'Grouping reads...'
+    #~ groups = []
+    #~ for read_name in sorted(read_dict):
+        #~ read = read_dict[read_name]
+        #~ make_new = True
+        #~ for group in groups:
+            #~ if group.check_membership(read):
+                #~ make_new = False
+                #~ break
+        #~ if make_new:
+            #~ groups.append(Group(read))
+        #~ 
+        #~ print 'Progress: {0}%'.format(int(float(i*100)/tot_reads)) 
+        #~ i += 1
+    #~ 
+    #~ with open('groups.txt', 'w') as out_handle:
+        #~ for group in groups:
+            #~ try:
+                #~ out_handle.write('j_{0} v_{1} size={2}\n'.format(
+                     #~ ref_names['J'][group.j], ref_names['V'][group.v], len(group.members)))
+            #~ except:
+                #~ print group.j, group.v    
+
+def parse_sams(j_sam, v_sam):
+    
+    # Input files with segment as key
+    in_files = {'J': j_sam,
+                'V': v_sam}
+    
+    # Dictionary to hold all read records
+    read_dict = {}
+    ref_names = {}
+    
+    for segment in in_files:
+    #~ for segment in ['V']:
+        with pysam.Samfile(in_files[segment], 'r') as sam_file:
+            
+            # Get names for the ref numbers
+            ref_names[segment] = get_ref_name_dict(sam_file)
+            
+            # Go through each read in the alignment
+            for alignedread in sam_file.fetch():
+                
+                # Skip if read is unmapped
+                if alignedread.is_unmapped:
+                    continue
+                
+                # Reads should be in reverse orientation (J -> V)
+                if not alignedread.is_reverse:
+                    print 'Warning! Mapping incorrect orientation: {0}'.format(alignedread.qname)
+                    continue
+                
+                #~ print 'List of pairs:\n{0}'.format(alignedread.aligned_pairs)
+                #~ print 'Mapping quality: {0}'.format(alignedread.mapq)
+                #~ print 'Ref start: {0}'.format(alignedread.pos)
+                #~ print 'Ref end + 1: {0}'.format(alignedread.aend)
+                #~ print 'Query name: {0}'.format(alignedread.qname)
+                #~ print 'Query start: {0}'.format(alignedread.qstart)
+                #~ print 'Query end from pairs: {0}'.format(alignedread.aligned_pairs[-1][0])
+                #~ print 'Query end: {0}'.format(alignedread.qend)
+                #~ print 'Query length: {0}'.format(alignedread.qlen)
+                #~ print 'Read length: {0}'.format(alignedread.rlen)
+                #~ print 'Inferred length: {0}'.format(alignedread.inferred_length)
+                #~ print 'T length: {0}'.format(alignedread.tlen)
+                #~ print 'Ref number: {0}'.format(alignedread.rname)
+                #~ print 'Ref name: {0}'.format(sam_file.getrname(alignedread.rname))
+                #~ print 'CIGAR str: {0}'.format(alignedread.cigarstring)
+                #~ print 'Ref lenght: {0}'.format(sam_file.lengths[alignedread.rname])
+                #~ print
+                #~ sys.exit()
+                
+                # Add additional attributes
+                ref_len = sam_file.lengths[alignedread.rname]
+                
+                # Make an instance for the read and link to it with record
+                if not alignedread.qname in read_dict:
+                    read_dict[alignedread.qname] = Read(alignedread.qname)
+                record = read_dict[alignedread.qname]
+                
+                # Parse alignment info
+                if segment == 'J':
+                    record.parse_J_attr(alignedread)
+                elif segment == 'V':
+                    record.parse_V_attr(alignedread, ref_len)
+                
+    return read_dict, ref_names
+
+def get_ref_name_dict(sam_file):
+    """ Given a sam file stream, it will return a dictionary of the reference
+        name for each reference number.
+    """
+    name_dict = {}
+    name_dict[None] = None
+    
+    for i in range(0, sam_file.nreferences):
+        name_dict[i] = sam_file.getrname(i)
+    
+    return name_dict
+
+if __name__ == '__main__':
+    
+    args = {}
+    
+    args['J_in'] = sys.argv[1]
+    args['V_in'] = sys.argv[2]
+    
+    main(args)
