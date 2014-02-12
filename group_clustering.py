d77aa751825e7b4305fa0b44eb0ada7a8707b93d
Ed Mountjoy <emountjoy@gmail.com>
2014-02-12 08:46:01 +0000

    done

--- group_clustering.py
+++ group_clustering.py
@@ -21,12 +21,14 @@ def main(args):
             read_names.append(header)
             read_seqs.append(seq)
     
+    size = 300
+    
     # Calculate distance matrix
     print 'Calculating distances...'
     dist_matrix = []
-    for seq in read_seqs:
+    for seq in read_seqs[:size]:
         seq_dists = []
-        for seq2 in read_seqs:
+        for seq2 in read_seqs[:size]:
             seq_dists.append(distance(seq, seq2))
         dist_matrix.append(seq_dists)
     dist_matrix = np.array(dist_matrix)
@@ -34,7 +36,7 @@ def main(args):
     # Perform DBSCAN
     print 'Clusting by DBSCAN...'
     db = DBSCAN(eps=2, min_samples=2, metric='precomputed').fit(dist_matrix)
-    print ' -Number of clusters: {0}'.format(len(set(db.labels_)))
+    print '- Number of clusters: {0}'.format(len(set(db.labels_)))
     
     # Do PCA
     print 'Doing PCA...'
@@ -42,12 +44,18 @@ def main(args):
     X = pca.transform(dist_matrix)
     
     # Plot PCA
-    colours = ['b', 'g', 'r', 'c', 'm', 'y', 'k']
-    fig = pl.figure(1, figsize=(4, 3))
+    colours =  pl.cm.get_cmap("rainbow", len(set(db.labels_))) 
+    fig = pl.figure()
     ax = Axes3D(fig)
     for row in range(X.shape[0]):
-        ax.scatter(X[row, 0], X[row, 1], X[row, 2],
-                   color=colours[int(db.labels_[row])])
+        # Get colour
+        label = int(db.labels_[row])
+        if label == -1:
+            col = 'k'
+        else:
+            col = colours(label)
+        # Plot
+        ax.scatter(X[row, 0], X[row, 1], X[row, 2], color=col)
     pl.show()    
 
 if __name__ == '__main__':
